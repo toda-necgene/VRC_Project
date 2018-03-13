3c038ce203639bc56be746e35bd37b329936101b
toda <tadoproject@gmail.com>
2018-03-14 08:33:32 +0900

    [update] ver 0.14.0 <datasetupdate>
    [add] 音素を考慮したデータセットの追加
    [fix] ShakeLayerの動作を高速化

--- VRC/train/Model/model2.py
+++ VRC/train/Model/model2.py
@@ -11,10 +11,10 @@ import pyaudio
 import random
 from datetime import datetime
 from tensorflow.python.ops import random_ops
+from tensorflow.python.ops import math_ops
 from tensorflow.python.layers import base
 from tensorflow.python.layers import utils
 import shutil
-import re
 from copy import deepcopy
 class Model:
     def __init__(self,debug):
@@ -24,10 +24,10 @@ class Model:
         self.down=128
         self.down_c=self.down-7
         self.up=64
-        self.input_ch=256
+        self.input_ch=1
         self.out_channels=self.down
         self.width=2
-        self.dataset_name="wave2wave_ver0.13.0"
+        self.dataset_name="wave2wave_ver0.14.0"
         self.data_format=[1,1,80000]
         f=open("Z://Data.txt",'w')
         f.write("Start:"+nowtime())
@@ -79,7 +79,7 @@ class Model:
         self.ans= tf.placeholder(tf.int16,
                                         [self.in_put_size[0], 1, self.out_put_size[2]],
                                         name='target_b')
-        self.inputs= tf.placeholder(tf.float32,
+        self.inputs= tf.placeholder(tf.int16,
                                         [self.in_put_size[0], 1, self.out_put_size[2]],
                                         name='target_d')
 
@@ -105,9 +105,9 @@ class Model:
                 for i in range(self.depth):
                     with tf.variable_scope('layer{}'.format(i)):
                         current = dict()
-                        w= tf.get_variable('w1a', [self.width,1,self.down_c,self.up],initializer=tf.contrib.layers.xavier_initializer())
+                        w= tf.get_variable('w1a', [self.width,1,self.down,self.up],initializer=tf.contrib.layers.xavier_initializer())
                         current['w-1'] =w
-                        w= tf.get_variable('w2a', [self.width,1,self.down_c,self.up],initializer=tf.contrib.layers.xavier_initializer())
+                        w= tf.get_variable('w2a', [self.width,1,self.down,self.up],initializer=tf.contrib.layers.xavier_initializer())
                         current['w-2'] =w
                         w = tf.get_variable('w3a', [1,1,self.up,self.down],initializer=tf.contrib.layers.xavier_initializer())
                         current['w-3'] =w
@@ -128,7 +128,7 @@ class Model:
                 self.var['postprocessing'] = current
                 self.var_pear.append(self.var)
 
-            self.fake_B ,self.fake_B_logit= self.generator(self.one_hot((self.real_A+1.0)/2.*255.0),self.one_hot((self.real_A+1.0)/2.*255.0),False,self.var_pear[0],"1",2)
+            self.fake_B ,self.fake_B_logit= self.generator(self.real_A*256.,False,self.var_pear[0],"1",1)
             self.fake_B_decoded=tf.stop_gradient(self.decode(self.un_oh(self.fake_B)),"asnyan")
 
 
@@ -145,9 +145,9 @@ class Model:
                 for i in range(self.depth):
                     with tf.variable_scope('layer{}'.format(i)):
                         current = dict()
-                        w= tf.get_variable('w1a', [self.width,1,self.down_c,self.up],initializer=tf.contrib.layers.xavier_initializer())
+                        w= tf.get_variable('w1a', [self.width,1,self.down,self.up],initializer=tf.contrib.layers.xavier_initializer())
                         current['w-1'] =w
-                        w= tf.get_variable('w2a', [self.width,1,self.down_c,self.up],initializer=tf.contrib.layers.xavier_initializer())
+                        w= tf.get_variable('w2a', [self.width,1,self.down,self.up],initializer=tf.contrib.layers.xavier_initializer())
                         current['w-2'] =w
                         w = tf.get_variable('w3a', [1,1,self.up,self.down],initializer=tf.contrib.layers.xavier_initializer())
                         current['w-3'] =w
@@ -168,7 +168,7 @@ class Model:
                 current['bias2']=bias
                 self.var['postprocessing'] = current
             self.var_pear.append(self.var)
-            self.fake_B_2 ,self.fake_B_logit_2= self.generator(self.one_hot((self.real_A+1.0)/2.*255.0),self.one_hot((self.real_A+1.0)/2.*255.0),False,self.var_pear[1],"2",1)
+            self.fake_B_2 ,self.fake_B_logit_2= self.generator(self.real_A*256.,False,self.var_pear[1],"2",2)
             self.fake_B_decoded_2=tf.stop_gradient(self.decode(self.un_oh(self.fake_B_2)),"asnyan")
 
         self.res1=tf.concat([self.inputs_result,self.real_data_result], axis=2)
@@ -213,8 +213,8 @@ class Model:
         l2g2=0.0
         l2d=0.0
         #l1=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.real_B, logits=self.fake_B))
-        self.g_loss_1 = lo+(tf.fill(lo.get_shape(), self.punish[1]))*(self.punish[0])+l2g1
-        self.g_loss_2 = lo2+(tf.fill(lo2.get_shape(), self.punish[1]))*(self.punish[0])+l2g2
+        self.g_loss_1 = lo+(tf.fill(lo.get_shape(), self.punish[1]))*(self.punish[0])*0.01+l2g1
+        self.g_loss_2 = lo2+(tf.fill(lo2.get_shape(), self.punish[1]))*(self.punish[0])*0.01+l2g2
         self.d_loss_R = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(self.d_judge_R), logits=self.d_judge_R_logits )
         self.d_loss_F = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(self.d_judge_F1), logits=self.d_judge_F1_logits )
         self.d_loss=tf.reduce_mean([self.d_loss_F,self.d_loss_R])+l2d
@@ -272,11 +272,11 @@ class Model:
         return otp.reshape(1,in_put.shape[1],in_put.shape[2]),otp2.reshape(1,in_put.shape[1],in_put.shape[2]),time.time()-tt
     def train(self,args):
         self.checkpoint_dir=args.checkpoint_dir
-        lr_g_opt=0.0001
+        lr_g_opt=0.00004
         beta_g_opt=0.9
-        lr_g_opt_2=0.00005
+        lr_g_opt_2=0.00002
         beta_g_opt_2=0.9
-        lr_d_opt=0.00004
+        lr_d_opt=0.000008
         beta_d_opt=0.1
         self.lod="[glr="+str(lr_g_opt)+",gb="+str(beta_g_opt)+"]"
         g_optim_1 = tf.train.AdamOptimizer(lr_g_opt,beta_g_opt).minimize(self.g_loss_1, var_list=self.g_vars_1)
@@ -326,13 +326,14 @@ class Model:
                 cur_res=np.zeros((self.batch_size,1,self.in_put_size[2]),dtype=np.int16)
                 cur_res_2=np.zeros((self.batch_size,1,self.in_put_size[2]),dtype=np.int16)
                 batch_files = data[idx*self.batch_size:(idx+1)*self.batch_size]
-                batch = [load_data(batch_file) for batch_file in batch_files]
-                batch_images = np.array(batch).astype(np.int16).reshape(self.batch_size,2,80000)
+                batch = np.asarray([load_data(batch_file) for batch_file in batch_files])
+                batch_images = np.array(batch).astype(np.int16).reshape(self.batch_size,2,24000)
+
                 test_train=load_data(data2[idx%2]).reshape(1,2,80000)
 
-                times=5*16000//self.out_put_size[2]+1
+                times=24000//self.out_put_size[2]
                 times_added=0
-                if (5*16000)%self.out_put_size[2]==0:
+                if int(24000)%self.out_put_size[2]==0:
                     times-=1
                 ti=(batch_idxs*times)//10+1
                 g_score=0
@@ -365,13 +366,14 @@ class Model:
                 # Update D network
 #                 self.sess.run(d_optim,feed_dict={self.real_data_result:resorce_te ,self.inputs_result:cv1,self.ans_result:target_te ,self.is_train:True })
                 hd,_=self.sess.run([self.d_loss_sum,d_optim],feed_dict={self.real_data_result:resorce_te , self.inputs_result:cv2,self.ans_result:target_te ,self.is_train:True })
+                hd,_=self.sess.run([self.d_loss_sum,d_optim],feed_dict={self.real_data_result:resorce_te , self.inputs_result:cv2,self.ans_result:target_te ,self.is_train:True })
                 self.writer.add_summary(hd, idx+batch_idxs*epoch)
                 d_score += (np.mean(p1s))
                 d_score += (np.mean(p2s))
                 print("D_score G1:%f %f G2:%f %f"%(np.mean(p1s),np.mean(score1),np.mean(p2s),np.mean(score2)))
                 dps+=(d_score)
                 for t in range(times):
-                    start_pos=self.out_put_size[2]*t+((5*16000)%self.out_put_size[2])
+                    start_pos=self.out_put_size[2]*t+(24000%self.out_put_size[2])
                     target=np.reshape(batch_images[:,0,max(0,start_pos-self.out_put_size[2]):start_pos],(self.batch_size,1,-1))
                     resorce=np.reshape(batch_images[:,1,max(0,start_pos-self.in_put_size[2]):start_pos],(self.batch_size,1,-1))
                     r=max(0,self.in_put_size[2]-resorce.shape[2])
@@ -412,15 +414,6 @@ class Model:
                     #Training D Netwok
                     b=random.randint(0,len(exp_re_2)-1)
                     cv2=exp_re_2[b]
-                    resorce_te_2=exp_re_r[b]
-                    target_te_2=exp_re_t[b]
-                    if counter % 10 == 0:
-#                         self.sess.run(d_optim,feed_dict={self.real_data_result:resorce_te_1 ,self.inputs_result:cv1,self.ans_result:target_te_1 ,self.is_train:True })
-                        _,hd=self.sess.run([d_optim,self.d_loss_sum],feed_dict={self.real_data_result:resorce_te_2 , self.inputs_result:cv2,self.ans_result:target_te_2 ,self.is_train:True })
-#                     elif counter % 20 == 2:
-#                         cvr=np.random.rand(self.data_format[0],self.data_format[1],self.data_format[2])*32767
-#                         cvr=cvr.astype(np.int16)
-#                         self.sess.run(d_optim,feed_dict={self.real_data_result:resorce_te ,self.inputs_result:cvr,self.ans_result:target_te ,self.is_train:True })
                 gps+=(g_score/times_added)
                 gps2+=(g_score2/times_added)
 
@@ -435,11 +428,11 @@ class Model:
             f=open(ff,'a')
             f.write("-------------------------------\n")
             f.write("TimeStamped:"+nowtime())
-            f.write("\nEpoch: [%2d]  time: %3.1f, \n G-LOSS_1: %f \n G-LOSS_2: %f \n D-Actually: %f \n test-g1 %f \n test-g2 %f \n" % (epoch+1,time.time() - start_time,(gps/batch_idxs),(gps2/batch_idxs),(dps/batch_idxs),(test1),(test2)))
+            f.write("\nEpoch: [%2d]  time: %3.1f, \n G-LOSS_1: %f \n G-LOSS_2: %f \n D-Actually: %f \n test-g1 %f \n test-g2 %f \n" % (epoch+1,time.time() - start_time,(gps),(gps2),(dps),(test1),(test2)))
             f.write("-------------------------------\n")
             f.close()
-            print("\nEpoch: [%2d]  time: %3.1f, \n G-LOSS_1: %f \n G-LOSS_2: %f \n D-Actually : %f \n" % (epoch+1,time.time() - start_time,(gps/batch_idxs),(gps2/batch_idxs),(dps/batch_idxs)))
-            rs=self.sess.run(self.rrs,feed_dict={ self.exps:out_put,self.exps_2:out_put2,self.g_loss_epo:(gps/batch_idxs),self.g_loss_epo_2:(gps2/batch_idxs),self.g_test_epo_1:(test1),self.g_test_epo_2:(test2)})
+            print("\nEpoch: [%2d]  time: %3.1f, \n G-LOSS_1: %f \n G-LOSS_2: %f \n D-Actually : %f \n" % (epoch+1,time.time() - start_time,(gps),(gps2),(dps)))
+            rs=self.sess.run(self.rrs,feed_dict={ self.exps:out_put,self.exps_2:out_put2,self.g_loss_epo:(gps),self.g_loss_epo_2:(gps2),self.g_test_epo_1:(test1),self.g_test_epo_2:(test2)})
             self.writer.add_summary(rs, epoch+1)
             print("test taken: %f secs" % (taken_time))
             upload(out_put_2)
@@ -451,13 +444,13 @@ class Model:
         mu=2**8-1.0
         ten=tf.to_float(in_puts)/32767.0
         inputs = tf.sign(ten,"sign2")*(tf.log(1+mu*tf.abs(ten),name="encode_log_up")/(tf.log(1+mu,name="encode_log_down")))
-        return tf.to_float(inputs)
-    def one_hot(self,inp):
-        inp=tf.cast(inp,tf.int32,name="integer")
-        ten=tf.one_hot(inp, 256, axis=-1)
-        ten=tf.reshape(ten, [self.batch_size,-1,256])
-        ten=tf.transpose(ten, perm=[0,2,1])
-        return tf.cast(ten,tf.float32)
+        return inputs
+#     def one_hot(self,inp):
+#         inp=tf.cast(inp,tf.int32,name="integer")
+#         ten=tf.one_hot(inp, 256, axis=-1)
+#         ten=tf.reshape(ten, [self.batch_size,-1,256])
+#         ten=tf.transpose(ten, perm=[0,2,1])
+#         return tf.cast(ten,tf.float32)
     def un_oh(self,in_puts):
         inputs=tf.reshape(in_puts, [self.batch_size,256,-1])
         ten=tf.transpose(inputs, perm=[0,2,1])
@@ -495,13 +488,13 @@ class Model:
         ot=tf.nn.sigmoid(ten)
         return ot,ten
 
-    def generator(self,in_put,current_outputs,reuse,var,name,sd):
+    def generator(self,current_outputs,reuse,var,name,sd):
         if reuse:
             tf.get_variable_scope().reuse_variables()
         else:
             assert tf.get_variable_scope().reuse == False
 
-        current_output=tf.reshape(current_outputs, [self.batch_size,256,self.in_put_size[2],1])
+        current_output=tf.reshape(current_outputs, [self.batch_size,self.input_ch,self.in_put_size[2],1])
 #         in_puts=tf.cast(in_put, tf.float32)
         #causual
         current = self.causal_layer(current_output,var,reuse,"causual_c"+name)
@@ -578,7 +571,7 @@ class Model:
             skp=tf.nn.conv2d(d8, w, [1,1,1,1], padding="VALID",data_format="NCHW",dilations=[1,1,1,1] ,name="dil_04"+name)
             skp=tf.reshape(skp,[self.batch_size,self.down,self.out_put_size[2]])
             if sd!=0 and depth%2==0:
-                otp=shake_drop(otp, rate=(depth)/(2*sd*self.depth),training=self.is_train,name="do_"+str(depth)+"-"+str(1)+name)
+                otp=shake_layer(otp, rate=(depth)/(2*sd*self.depth),var=(depth)/(sd*self.depth),training=self.is_train,name="do_"+str(depth)+"-"+str(1)+name)
             return skp,otp+con
 
     def save(self, checkpoint_dir, step):
@@ -609,25 +602,25 @@ class Model:
 
 def dilation_conv(inp,w,w2,name,width,otc,s):
     in_s=(inp.get_shape())
-    ten=tf.reshape(inp,[in_s[0],in_s[1],in_s[2],in_s[3]])
-    ten=tf.transpose(ten, [0,2,1,3])
-    ten=tf.nn.conv2d(ten, w2, strides=[1,1,1,1], padding="VALID",  data_format="NCHW", name=name+"-CH")
-    ten=tf.transpose(ten, [0,2,1,3])
-    ten=tf.reshape(ten,[in_s[0],s,in_s[2],in_s[3]])
-    ten=tf.nn.conv2d(ten, w, [1,1,1,1], padding="VALID",data_format="NCHW" ,name=name)
+#     ten=tf.reshape(inp,[in_s[0],in_s[1],in_s[2],in_s[3]])
+#     ten=tf.transpose(ten, [0,2,1,3])
+#     ten=tf.nn.conv2d(ten, w2, strides=[1,1,1,1], padding="VALID",  data_format="NCHW", name=name+"-CH")
+#     ten=tf.transpose(ten, [0,2,1,3])
+#     ten=tf.reshape(ten,[in_s[0],s,in_s[2],in_s[3]])
+    ten=tf.nn.conv2d(inp, w, [1,1,1,1], padding="VALID",data_format="NCHW" ,name=name)
     in_s=(ten.get_shape())
     ten=tf.reshape(ten,[in_s[0],otc,width,in_s[2]//width,in_s[3]])
     ten=tf.transpose(ten, [0,1,3,2,4])
     ten=tf.reshape(ten,[in_s[0],otc,in_s[2]//width,in_s[3]*width])
     return ten
-def shake_drop(in_p,rate,training,name):
-    layer=Shake_Dropout(rate,name)
+def shake_layer(in_p,rate,var,training,name):
+    layer=Shake_Layer(rate,var,name)
     return layer.apply(in_p,training=training)
-class Shake_Dropout(base.Layer):
-    def __init__(self,rate,name,**kwargs):
-        super(Shake_Dropout, self).__init__(name=name, **kwargs)
-        self.rate=1-rate
-
+class Shake_Layer(base.Layer):
+    def __init__(self,rate,var,name,**kwargs):
+        super(Shake_Layer, self).__init__(name=name, **kwargs)
+        self.rate=rate
+        self.var=var
 
     def call(self,inputs,training=False):
         def dropped_inputs():
@@ -636,24 +629,16 @@ class Shake_Dropout(base.Layer):
             ps2=int(inputs.shape[2])
             ps3=int(inputs.shape[3])
             b=self.rate+random_ops.random_uniform([ps0,1,1,1], name=self.name+"SDR_1")
-            b=tf.tile(b, [1,ps1,ps2,ps3])
-            bel=tf.floor(b)
-            @tf.RegisterGradient(self.name+"Gradient_Ident")
-            def _change_grad(op,grad):
-                tenx=(random_ops.random_uniform([ps0,ps1,ps2,ps3],name=self.name))
-                return ((1-bel)*grad*tenx,grad*op.inputs[0])
-
-            ten1=bel*inputs
-            ten2=(1-bel)*inputs
-            ten2_beta=(random_ops.random_uniform([ps0,ps1,ps2,ps3],name=self.name)*2-1)
-            g=tf.get_default_graph()
-            with g.gradient_override_map({"Mul": self.name+"Gradient_Ident"}):
-                ten3=tf.multiply(ten2,ten2_beta,"Multiply")
+            bel=math_ops.floor(b)
+            ten1=inputs
+            ten2=(bel)*inputs
+            ten2_beta=(random_ops.random_uniform([ps0,ps1,ps2,ps3],name=self.name)*2.-1.)*self.var
+            ten3=ten2*ten2_beta
             ten=ten1+ten3
             return ten
         return utils.smart_cond(training,
                             dropped_inputs,
-                            lambda: tf.identity(inputs*(self.rate)))
+                            lambda: tf.identity(inputs))
 
 def nowtime():
     return datetime.now().strftime("%Y_%m_%d %H_%M_%S")
@@ -681,4 +666,5 @@ def imread(path):
         ans=np.append(ans,np.frombuffer(bb,"int16"))
         bb=wf.readframes(1024)
     wf.close()
-    return ans[0:160000]
+    i=ans.shape[0]//16000
+    return ans[0:i*16000]
--- VRC/utils/voice.py
+++ VRC/utils/voice.py
@@ -21,93 +21,36 @@ RECORD_SECONDS = 5 #録音する時間の長さ
 WAVE_INPUT_FILENAME = "../train/Model/datasets/source/"
 WAVE_TMP="tmp.wav"
 WAVE_OUTPUT_FILENAME = "../train/Model/datasets/train/01/"
-files=glob.glob(WAVE_INPUT_FILENAME+"/*.wav")
+file=(WAVE_INPUT_FILENAME+"/結合済み.wav")
 index=0
 dms=[]
-
-wf=wave.open(files[0],'rb')
-dds=wf.readframes(CHUNK)
-while dds !=b'':
+wf = wave.open(file, 'rb')
+dds = wf.readframes(CHUNK)
+while dds != b'':
     dms.append(dds)
-    dds=wf.readframes(CHUNK)
-dms=b''.join(dms)
-data=np.frombuffer(dms,'int16')[0:160000]
-data2=np.reshape(data, [2,50,1600])
-data_realA=data2[0]
-data_realB=data2[1]
-p=pyaudio.PyAudio()
-datanum=np.append(data_realA,data_realB)
-ww = wave.open("../train/Model/datasets/train/02/tt.wav", 'wb')
-ww.setnchannels(1)
-ww.setsampwidth(p.get_sample_size(FORMAT))
-ww.setframerate(RATE)
-ww.writeframes(datanum.tobytes())
-ww.close()
-wf=wave.open(files[1],'rb')
-dds=wf.readframes(CHUNK)
-dms=[]
-while dds !=b'':
-    dms.append(dds)
-    dds=wf.readframes(CHUNK)
-dms=b''.join(dms)
-data=np.frombuffer(dms,'int16')[0:160000]
-data2=np.reshape(data, [2,50,1600])
-data_realA=data2[0]
-data_realB=data2[1]
-p=pyaudio.PyAudio()
-datanum=np.append(data_realA,data_realB)
-ww = wave.open("../train/Model/datasets/train/02/tt2.wav", 'wb')
-ww.setnchannels(1)
-ww.setsampwidth(p.get_sample_size(FORMAT))
-ww.setframerate(RATE)
-ww.writeframes(datanum.tobytes())
-ww.close()
-for file in files[2:-1]:
-    dms=[]
-    wf=wave.open(file,'rb')
-    dds=wf.readframes(CHUNK)
-    while dds !=b'':
-        dms.append(dds)
-        dds=wf.readframes(CHUNK)
-    dms=b''.join(dms)
-    data=np.frombuffer(dms,'int16')[0:160000]
-    data2=np.reshape(data, [2,50,1600])
-    data_realA=data2[0]
-    data_realB=data2[1]
-    p=pyaudio.PyAudio()
-    for i in range(20):
-        ##音声加工
+    dds = wf.readframes(CHUNK)
+dms = b''.join(dms)
+data = np.frombuffer(dms, 'int16')[0:4480000].reshape(2,-1)
+data_realA=data[0]
+data_realB=data[1]
+time=80000
+times=data_realA.shape[0]//time
+mod=80000//5
+for tt in range(5):
+    starttime=mod*tt
+    for i in range(times):
+        ##音声抽出
         star=random.randint(0,5)
         dur=random.randint(15,25)
-        data_A_N = deepcopy(data_realA)
-        data_B_N=deepcopy(data_realB)
-        sss=random.randint(1,len(files)-1)
-        dms = []
-        wf = wave.open(files[sss], 'rb')
-        dds = wf.readframes(CHUNK)
-        while dds != b'':
-            dms.append(dds)
-            dds = wf.readframes(CHUNK)
-        dms = b''.join(dms)
-        data = np.frombuffer(dms, 'int16')[0:160000]
-        data2 = np.reshape(data, [2, 50, 1600])
-        data_realA_2 = data2[0]
-        data_realB_2 = data2[1]
-
-        #data_B_N.flags.writable=True
-        if i!=0:
-            for s in range(star,dur):
-                if (star+dur)<50:
-                    data_A_N[s]=data_realA_2[s]
-                    data_B_N[s] = data_realB_2[s]
-        ##ダウンサンプリング
-        fs_target = 8000
-        ##保存
+        data_A_N = deepcopy(data_realA)[i*time+starttime:(i+1)*time+starttime]
+        data_B_N=deepcopy(data_realB)[i*time+starttime:(i+1)*time+starttime]
+        sss=random.randint(1,5-1)
+        #保管
+        p=pyaudio.PyAudio()
         datanum=np.append(data_A_N,data_B_N)
-        ww = wave.open(WAVE_OUTPUT_FILENAME+str(index)+"-"+str(i+1)+"-"+str(0)+".wav", 'wb')
+        ww = wave.open(WAVE_OUTPUT_FILENAME+str(tt+1)+"-"+str(i+1)+".wav", 'wb')
         ww.setnchannels(1)
         ww.setsampwidth(p.get_sample_size(FORMAT))
         ww.setframerate(RATE)
         ww.writeframes(datanum.tobytes())
         ww.close()
-    index+=1
