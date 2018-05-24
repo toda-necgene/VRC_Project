41d665831292f91b4d35c3b20475f3f704529414
toda <tadoproject@gmail.com>
2018-05-24 23:35:34 +0900

    [update] test update <version 2.2.0>
    [change] tanhを位相スペクトラムにのみかけるようにした。また、PI倍している
    []

--- VRC/train/Model/model_proto.py
+++ VRC/train/Model/model_proto.py
@@ -23,6 +23,7 @@ class Model:
         self.args["train_data_num"]=500
         self.args["batch_size"]=1
         self.args["depth"] =4
+        self.args["d_depth"] = 4
         self.args["train_epoch"]=500
         self.args["test"]=True
         self.args["log"] = True
@@ -37,9 +38,12 @@ class Model:
         self.args["cupy"] = False
         self.args["D_channels"] =[2]
         self.args["G_channel"] = 32
+        self.args["G_channel2"] = 32
         self.args["strides_g"] = [2,2]
+        self.args["strides_g2"] = [1, 1]
         self.args["strides_d"] = [2,2]
         self.args["filter_g"] = [8,8]
+        self.args["filter_g2"] = [2, 2]
         self.args["filter_d"] = [4,4]
         self.args["model_name"] = "wave2wave"
         self.args["version"] = "1.0.0"
@@ -72,9 +76,9 @@ class Model:
                  print(' [x] JSONDecodeError: ', e)
         else:
             print( " [!] Setting file is not found")
-        if len(self.args["D_channels"]) != (self.args['depth'] + 1):
-            print(" [!] Channels length and depth+1 must be equal ." + str(len(self.args["D_channels"])) + "vs" + str(self.args['depth'] + 1))
-            self.args["D_channels"] = [min([2 ** (i + 1) - 2, 254]) for i in range(self.args['depth'] + 1)]
+        if len(self.args["D_channels"]) != (self.args['d_depth'] + 1):
+            print(" [!] Channels length and depth+1 must be equal ." + str(len(self.args["D_channels"])) + "vs" + str(self.args['d_depth'] + 1))
+            self.args["D_channels"] = [min([2 ** (i + 1) - 2, 254]) for i in range(self.args['d_depth'] + 1)]
         if self.args["pitch_rate"]==1.0:
             self.args["pitch_rate"] = self.args["pitch_tar"]/self.args["pitch_res"]
             print(" [!] pitch_rate is not found . calculated value : "+str(self.args["pitch_rate"]))
@@ -107,8 +111,12 @@ class Model:
         #creating generator
         #G-net（生成側）の作成
         with tf.variable_scope("generator_1"):
-            self.fake_B_image=generator(self.input_model, reuse=False,chs=self.args["G_channel"],depth=self.args["depth"],f=self.args["filter_g"],s=self.args["strides_g"])
-
+            ipt=self.input_size_model[0:3]
+            ipt.append(1)
+            self.fake_B_image1=generator(tf.reshape(self.input_model[:,:,:,0],ipt), reuse=False,chs=self.args["G_channel"],depth=self.args["depth"],f=self.args["filter_g"],s=self.args["strides_g"],activate=False)
+            self.fake_B_image2 = generator(tf.reshape(self.input_model[:,:,:,1],ipt), reuse=False, chs=self.args["G_channel"],
+                                          depth=self.args["depth"], f=self.args["filter_g2"], s=self.args["strides_g2"],activate=True)
+            self.fake_B_image=tf.concat([self.fake_B_image1,self.fake_B_image2],3)
         self.noise = tf.placeholder(tf.float32, [self.args["batch_size"]], "inputs_Noise")
 
         b_true_noised=self.input_model_label+tf.random_normal(self.input_model_label.shape,0,self.noise[0])
@@ -119,8 +127,8 @@ class Model:
         #creating discriminator
         #D-net（判別側)の作成
         with tf.variable_scope("discrim",reuse=tf.AUTO_REUSE):
-            self.d_judge_F1=discriminator(self.res1,False,self.args["filter_d"],self.args["strides_d"],self.args["depth"],self.args["D_channels"])
-            self.d_judge_R=discriminator(self.res2,True,self.args["filter_d"],self.args["strides_d"],self.args["depth"],self.args["D_channels"])
+            self.d_judge_F1,self.d_judge_F1_logits=discriminator(self.res1,False,self.args["filter_d"],self.args["strides_d"],self.args["d_depth"],self.args["D_channels"])
+            self.d_judge_R,self.d_judge_R_logits=discriminator(self.res2,True,self.args["filter_d"],self.args["strides_d"],self.args["d_depth"],self.args["D_channels"])
 
         #getting individual variabloes
         #それぞれの変数取得
@@ -133,15 +141,15 @@ class Model:
         #L1 norm loss
         L1=tf.reduce_mean(tf.pow(self.input_model_label-self.fake_B_image,2)*0.5)
         #Gan loss
-        DS=tf.reduce_mean(-tf.log(self.d_judge_F1))
+        DS=tf.reduce_mean(-tf.log(self.d_judge_F1+1e-8))
         #generator loss
         self.g_loss_1=L1*100+DS
-
-        a=1.0-self.noise
+        c=1.0-self.noise
+        a=tf.reshape(c,self.d_judge_F1_logits.shape)
         #objective-functions of discriminator
         #D-netの目的関数
-        self.d_loss_R = tf.reduce_mean(jsd(self.d_judge_R,a))
-        self.d_loss_F = tf.reduce_mean(jsd(self.d_judge_F1,0.0))
+        self.d_loss_R = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=a,logits=self.d_judge_R_logits))
+        self.d_loss_F = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros(self.d_judge_F1_logits.shape),logits=self.d_judge_F1_logits))
 
         #tensorboard functions
         #tensorboard 表示用関数
@@ -150,8 +158,8 @@ class Model:
         self.dscore = tf.summary.scalar("dscore", tf.reduce_mean(self.d_judge_F1))
         self.g_loss_sum_1= tf.summary.merge([self.g_loss_all,self.g_loss_gan,self.dscore])
         self.d_loss_sum = tf.summary.merge([tf.summary.scalar("d_loss", tf.reduce_mean(self.d_loss_R)),tf.summary.scalar("d_loss_F", tf.reduce_mean(self.d_loss_F))])
-        self.result=tf.placeholder(tf.float32, [1,1,160000], name="FB")
-        self.fake_B_sum = tf.summary.audio("fake_B", tf.reshape(self.result,[1,160000,1]), 16000, 1)
+        self.result=tf.placeholder(tf.float32, [1,1,320000], name="FB")
+        self.fake_B_sum = tf.summary.audio("fake_B", tf.reshape(self.result,[1,320000,1]), 32000, 1)
         self.g_test_epo=tf.placeholder(tf.float32,name="g_test_epoch_end")
         self.g_test_epo_2 = tf.placeholder(tf.float32, name="g_test_epoch_end_2")
         self.g_test_epoch = tf.summary.merge([tf.summary.scalar("g_test_epoch_end", self.g_test_epo),tf.summary.scalar("g_related_score", self.g_test_epo_2)])
@@ -177,7 +185,6 @@ class Model:
             times-=1
         otp=np.array([],dtype=np.int16)
         otp2 = np.array([], dtype=np.int16)
-
         for t in range(times):
             # Preprocess
             # 前処理
@@ -189,7 +196,7 @@ class Model:
             resorce=np.reshape(in_put[0,max(0,start_pos-ipt):start_pos,0],(1,-1))
             r=max(0,ipt-resorce.shape[1])
             if r>0:
-                resorce=np.pad(resorce,((0,0),(r,0)),'reflect')
+                resorce=np.pad(resorce,((0,0),(r,0)),'constant')
             red=np.append(resorce,red)
             red=red.reshape((self.args["batch_size"],ipt))
             res = np.zeros(self.input_size_model)
@@ -198,9 +205,6 @@ class Model:
             # ピッチ変更
             for i in range(self.args["batch_size"]):
                 red[i] = shift(red[i] / 32767.0, self.args["pitch_rate"]).reshape(red[i].shape)
-                r = self.args["SHIFT"] - res[i].shape[0] % self.args["SHIFT"]
-                if r != self.args["SHIFT"]:
-                    red[i] = np.pad(red[i], (0, r), "reflect")
 
             # FFT
             # 短時間高速離散フーリエ変換
@@ -211,7 +215,7 @@ class Model:
             # running network
             # ネットワーク実行
             res=self.sess.run(self.fake_B_image,feed_dict={ self.input_model:res})
-
+            # resas = np.append(resas, res[0])
 
             # Postprocess
             # 後処理
@@ -221,11 +225,21 @@ class Model:
             res=self.ifft(res[0])*32767/2
             # chaching results
             # 結果の保存
-            res=res.reshape(-1)
+            res=res.reshape(-1).astype(np.int16)
             otp=np.append(otp,res)
         h=otp.shape[0]-in_put.shape[1]-1
         if h>0:
-            otp=otp[h:-1]
+            otp=otp[1:-h]
+        # resas=resas[1:].reshape(-1,self.args["NFFT"],2)
+        # plt.subplot(4, 1, 1)
+        # abn = np.transpose(resas[ :, :, 0], (1, 0))
+        # plt.imshow(abn,aspect="auto")
+        # plt.colorbar()
+        # plt.subplot(4, 1, 2)
+        # abn = np.transpose(resas[:, :, 1], (1, 0))
+        # plt.imshow(abn,aspect="auto")
+        # plt.colorbar()
+
         return otp.reshape(1,in_put.shape[1],in_put.shape[2]),time.time()-tt
 
 
@@ -283,6 +297,7 @@ class Model:
         # loading test data
         # テストデータの読み込み
         test=imread('./Model/datasets/test/test.wav')[0:160000]
+        test2_s = imread('./Model/datasets/test/test2.wav')[0:160000]
         label=imread('./Model/datasets/test/label.wav')[0:160000]
 
         # times of one epoch
@@ -313,7 +328,12 @@ class Model:
                 #testing
                 #テスト
                 out_puts,taken_time=self.convert(test.reshape(1,-1,1))
-
+                # plt.subplot(4, 1, 3)
+                # plt.plot(test[ :])
+                # plt.subplot(4, 1, 4)
+                # plt.plot(out_puts[0,:,0])
+                # plt.show()
+                out_puts2 ,_ = self.convert(test2_s.reshape(1, -1, 1))
                 if test_before is not None:
                     aaa=self.testact(test_before.reshape(1,-1,1),label.reshape(1,-1,1))
                     bbb=self.testact(out_puts.reshape(1,-1,1), label.reshape(1,-1,1))
@@ -322,7 +342,7 @@ class Model:
                     bbb = self.testact(out_puts.reshape(1, -1, 1), label.reshape(1, -1, 1))
                     test2 = bbb
                 test_before=out_puts
-                out_put=(out_puts.astype(np.float32)/32767.0)
+                out_put=np.append(out_puts.astype(np.float32)/32767.0,out_puts2.astype(np.float32)/32767.0)
 
                 # loss of tesing
                 #テストの誤差
@@ -342,7 +362,7 @@ class Model:
                 #テストの結果の保存
                 if os.path.exists(self.args["wave_otp_dir"]):
                     upload(out_puts,self.args["wave_otp_dir"])
-
+                    upload(out_puts2, self.args["wave_otp_dir"],comment="welknown")
             print(" [*] Epoch %3d started" % epoch)
 
             for idx in xrange(0, batch_idxs):
@@ -364,8 +384,7 @@ class Model:
 
 
 
-                ti=(batch_idxs*times//self.args["train_interval"])+1
-                tm=time.time()
+                ti=(batch_idxs*times)
                 for t in time_set:
                     tm = time.time()
                     # calculating starting position
@@ -385,10 +404,10 @@ class Model:
                     # サイズ合わせ
                     r=max(0,ipt-resorce.shape[1])
                     if r>0:
-                        resorce=np.pad(resorce,((0,0),(r,0)),'reflect')
+                        resorce=np.pad(resorce,((0,0),(r,0)),'constant')
                     r=max(0,ipt-target.shape[1])
                     if r>0:
-                        target=np.pad(target,((0,0),(r,0)),'reflect')
+                        target=np.pad(target,((0,0),(r,0)),'constant')
 
 
                     # changing pitch
@@ -396,9 +415,6 @@ class Model:
                     res = np.zeros(resorce.shape)
                     for i in range(self.args["batch_size"]):
                         res[i] = shift(resorce[i],self.args["pitch_rate"]).reshape(resorce[i].shape)
-                        r = self.args["SHIFT"] - res[i].shape[0] % self.args["SHIFT"]
-                        if r != self.args["SHIFT"]:
-                            res[i] = np.pad(res[i], (0, r), "reflect")
 
                     # FFT
                     # 短時間高速離散フーリエ変換
@@ -424,8 +440,8 @@ class Model:
                     if self.args["tensorboard"] and counter%self.args["train_interval"]==0:
                         nos = np.random.rand(self.args["batch_size"]) * 0.5
                         hg,hd=self.sess.run([self.g_loss_sum_1,self.d_loss_sum],feed_dict={self.input_model:res_t, self.input_model_label:tar ,self.noise:nos  })
-                        self.writer.add_summary(hg, counter//self.args["train_interval"]+ti*epoch)
-                        self.writer.add_summary(hd, counter//self.args["train_interval"]+ti*epoch)
+                        self.writer.add_summary(hg, counter+ti*epoch)
+                        self.writer.add_summary(hd, counter+ti*epoch)
                     if self.args["stop_argument"] and counter % self.args["train_interval"] == 0:
                         nos = np.random.rand(self.args["batch_size"]) * 0.5
                         hg,hd = self.sess.run([self.g_loss_1,self.d_judge_F1], feed_dict={self.input_model: res_t, self.input_model_label: tar,self.noise:nos })
@@ -525,6 +541,7 @@ class Model:
         c = np.log(np.power(re, 2) + np.power(im, 2) + 1e-24).reshape(time_ruler, -1, 1)
         d = np.arctan2(im, re).reshape(time_ruler, -1, 1)
         spec = np.concatenate((c, d), 2)
+
         return spec
     def ifft(self,data):
         a=np.clip(data[:, :, 0],a_min=-100000,a_max=88)
@@ -637,14 +654,6 @@ class Model:
                 f.write("\n %10s : %5.5f  :%5.5f" % (i,float(np.min(otp)),float(np.mean(otp))))
                 f.flush()
 
-#model architectures
-def kld(p,q):
-    eps=1e-8
-    return tf.reduce_sum(p*tf.log(p/(q+eps)+eps))
-
-def jsd(p,q):
-    m=(p+q)*0.5
-    return 0.5*(kld(p,m)+kld(q,m))
 
 def discriminator(inp,reuse,f,s,depth,chs):
     current=tf.cast(inp, tf.float32)
@@ -654,8 +663,8 @@ def discriminator(inp,reuse,f,s,depth,chs):
         current = tf.nn.leaky_relu(ten)
     h4=tf.reshape(current, [current.shape[0],-1])
     ten=tf.layers.dense(h4,1,name="dence",reuse=reuse)
-    return tf.nn.sigmoid(ten)
-def generator(current_outputs,reuse,depth,chs,f,s):
+    return tf.nn.sigmoid(ten),ten
+def generator(current_outputs,reuse,depth,chs,f,s,activate=False):
     if reuse:
         tf.get_variable_scope().reuse_variables()
     else:
@@ -681,10 +690,12 @@ def generator(current_outputs,reuse,depth,chs,f,s):
 
         current = ten + connections
     ca=current.shape
-    x=tf.constant([[[[88.0]]]])
-    x2=tf.constant([[[[math.pi]]]])
-    a=tf.nn.tanh(current)*tf.concat([tf.tile(x,[int(ca[0]),int(ca[1]),int(ca[2]),1]),tf.tile(x2,[int(ca[0]),int(ca[1]),int(ca[2]),1])],3)
-    return a
+
+    if activate:
+        x2=tf.constant([[[[math.pi]]]])
+        current=tf.nn.tanh(current)*tf.tile(x2,[int(ca[0]),int(ca[1]),int(ca[2]),1])
+
+    return current
 
 def shift(data_inps,pitch):
     data_inp=data_inps.reshape(-1)
@@ -699,36 +710,38 @@ def scale(inputs,len_wave):
     return spec.reshape(-1)
 def time_strech(datanum,speed):
     term_s = int(16000 * 0.05)
+    fade=term_s//2
     pulus=int(term_s*speed)
     data_s=datanum.reshape(-1)
     spec=np.zeros(1)
-    ifs=np.zeros(pulus//2)
+    ifs=np.zeros(fade)
     for i_s in np.arange(0.0,data_s.shape[0],pulus):
-        dd=data_s[int(i_s):int(i_s+term_s)]
-        fade = min(int(pulus/2),dd.shape[0])
-        ds_in= np.linspace(0,1,fade)
-        ds_out =  np.linspace(1,0,fade)
-        stock=dd[:fade]
-        dd[:fade] = dd[:fade] * ds_in
-        if  i_s!=0:
-            dd[:fade]+=ifs[:fade]
-        else :
-            dd[:fade] += stock * np.linspace(1, 0, fade)
-        ifs=dd[-fade:]*ds_out
-        if i_s+pulus>=data_s.shape[0]:
+        st=int(i_s)
+        fn=min(int(i_s+term_s+fade),data_s.shape[0])
+        dd=data_s[st:fn]
+        if i_s + pulus >= data_s.shape[0]:
             spec = np.append(spec, dd)
         else:
+            ds_in = np.linspace(0, 1, fade)
+            ds_out = np.linspace(1, 0, fade)
+            stock = dd[:fade]
+            dd[:fade] = dd[:fade] * ds_in
+            if st != 0:
+                dd[:fade] += ifs[:fade]
+            else:
+                dd[:fade] += stock * np.linspace(1, 0, fade)
+            if fn!=data_s.shape[0]:
+                ifs = dd[-fade:] * ds_out
             spec=np.append(spec,dd[:-fade])
     return spec[1:]
-
 def nowtime():
     return datetime.now().strftime("%Y_%m_%d %H_%M_%S")
 
-def upload(voice,to):
+def upload(voice,to,comment=""):
     voiced=voice.astype(np.int16)
     p=pyaudio.PyAudio()
     FORMAT = pyaudio.paInt16
-    ww = wave.open(to+nowtime()+".wav", 'wb')
+    ww = wave.open(to+nowtime()+comment+".wav", 'wb')
     ww.setnchannels(1)
     ww.setsampwidth(p.get_sample_size(FORMAT))
     ww.setframerate(16000)
--- VRC/utils/voice.py
+++ VRC/utils/voice.py
@@ -2,9 +2,10 @@ import pyaudio
 import numpy as np
 import wave
 import matplotlib.pyplot as pl
+import matplotlib.cm as cm
 import cupy
 import time
-NFFT=128
+NFFT=1024
 SHIFT=NFFT//2
 C1=32.703
 rate=16000
@@ -25,21 +26,12 @@ def fft(data):
         wined[fft_index]=frame*window
         pos += NFFT // 2
     wineds=cupy.asarray(wined,dtype=cupy.float64)
-    print(wineds.shape)
     fft_rs=cupy.fft.fft(wineds,n=NFFT,axis=-1)
     fft_r=cupy.asnumpy(fft_rs)
-
-    re=fft_r.real.reshape(time_ruler,-1)
-    im=fft_r.imag.reshape(time_ruler,-1)
-    c = np.log(np.power(re, 2) + np.power(im, 2) + 1e-24).reshape(time_ruler,-1,1)
-    d = np.arctan2(im, re).reshape(time_ruler,-1,1)
-    spec=np.concatenate((c,d),2)
-    return spec
+    return fft_r.reshape(time_ruler, -1)
 def shift(data_inps,pitch):
     data_inp=data_inps.reshape(-1)
     return scale(time_strech(data_inp,1/pitch),data_inp.shape[0])
-    # return time_strech(data_inp,1/pitch)
-    # return scale(data_inp,data_inp.shape[0]/2)
 
 def scale(inputs,len_wave):
     x=np.linspace(0.0,inputs.shape[0]-1,len_wave)
@@ -48,36 +40,50 @@ def scale(inputs,len_wave):
     return spec.reshape(-1)
 def time_strech(datanum,speed):
     term_s = int(rate * 0.05)
+    fade=term_s//2
     pulus=int(term_s*speed)
     data_s=datanum.reshape(-1)
     spec=np.zeros(1)
-    ifs=np.zeros(pulus//2)
+    ifs=np.zeros(fade)
     for i_s in np.arange(0.0,data_s.shape[0],pulus):
-        dd=data_s[int(i_s):int(i_s+term_s)]
-        fade = min(int(pulus/2),dd.shape[0])
-        ds_in= np.linspace(0,1,fade)
-        ds_out =  np.linspace(1,0,fade)
-        stock=dd[:fade]
-        dd[:fade] = dd[:fade] * ds_in
-        if  i_s!=0:
-            dd[:fade]+=ifs[:fade]
-        else :
-            dd[:fade] += stock * np.linspace(1, 0, fade)
-        ifs=dd[-fade:]*ds_out
-        if i_s+pulus>=data_s.shape[0]:
+        st=int(i_s)
+        fn=min(int(i_s+term_s+fade),data_s.shape[0])
+        dd=data_s[st:fn]
+        if i_s + pulus >= data_s.shape[0]:
             spec = np.append(spec, dd)
         else:
+            ds_in = np.linspace(0, 0.999, fade)
+            ds_out = np.linspace(0.999, 0, fade)
+            stock = dd[:fade]
+            dd[:fade] = dd[:fade] * ds_in
+            if st != 0:
+                dd[:fade] += ifs[:fade]
+            else:
+                dd[:fade] += stock * np.linspace(0.999, 0, fade)
+            if fn!=data_s.shape[0]:
+                ifs = dd[-fade:] * ds_out
             spec=np.append(spec,dd[:-fade])
     return spec[1:]
 
+def complex_to_pp(fft_r):
+    time_ruler=fft_r.shape[0]
+    re = fft_r.real
+    im = fft_r.imag
+    c = np.log(np.power(re, 2) + np.power(im, 2) + 1e-24).reshape(time_ruler, -1, 1)
+    d = np.arctan2(im, re).reshape(time_ruler, -1, 1)
+    spec = np.concatenate((c, d), 2)
+    return spec
+
+def pp_to_complex(frame):
+    power = np.sqrt(np.exp(frame[:, :, 0]))
+    re = power * (np.cos(frame[:, :, 1]))
+    im = power * (np.sin(frame[:, :, 1]))
+    ep = re + 1j * im
+    return ep
+
 def ifft(data):
     window=np.hamming(NFFT)
-    frame=data
-    power = np.sqrt(np.exp(frame[:,:,0]))
-    re = power * (np.cos(frame[:,:,1]))
-    im = power * (np.sin(frame[:,:,1]))
-    ep = re+1j*im
-    eep=cupy.asarray(ep)
+    eep=cupy.asarray(data)
     fft_s=cupy.fft.ifft(eep)
     fft_s=cupy.asnumpy(fft_s)
     fft_data=fft_s.real
@@ -92,10 +98,10 @@ CHANNELS = 1        #モノラル
 RATE = 16000       #サンプルレート
 CHUNK = 1024     #データ点数
 RECORD_SECONDS = 5 #録音する時間の長さ
-WAVE_INPUT_FILENAME = "../train/Model/datasets/test/"
-WAVE_TMP="tmp.wav"
-WAVE_OUTPUT_FILENAME = "../train/Model/datasets/train/01/"
-file=(WAVE_INPUT_FILENAME+"/test.wav")
+WAVE_INPUT_FILENAME = "./"
+WAVE_OUTPUT_FILENAME = "../train/Model/datasets/test/test.wav"
+file=(WAVE_INPUT_FILENAME+"noise.wav")
+file=WAVE_OUTPUT_FILENAME
 index=0
 dms=[]
 wf = wave.open(file, 'rb')
@@ -104,16 +110,17 @@ while dds != b'':
     dms.append(dds)
     dds = wf.readframes(CHUNK)
 dms = b''.join(dms)
-data = np.frombuffer(dms, 'int16')[0:160000].reshape(2,-1)
+data = np.frombuffer(dms, 'int16')
 data_realA=data.reshape(-1)
+print(data_realA.shape)
 timee=80000
 times=data_realA.shape[0]//timee
 
 rate=16000
 
 b=np.zeros([1])
-ab=np.zeros([1,128,2])
-abc=np.zeros([1,128,2])
+ab=np.zeros([1,1024,2])
+abc=np.zeros([1,1024,2])
 
 term=8192
 times=data_realA.shape[0]//term+1
@@ -133,24 +140,23 @@ for i in range(times):
     if r!=SHIFT:
         dmn=np.pad(dmn,(0,r),"reflect")
     a=fft(dmn)
+    a=complex_to_pp(a)
+    abc = np.append(abc, a, axis=0)
     ab = np.append(ab, a, axis=0)
+    a=pp_to_complex(a)
     s=ifft(a)
     b=np.append(b,s)
 # print(a)
-print(time.time()-ttm)
 r=b.shape[0]-data_realA.shape[0]
 b=(b[1:]/2*32767).astype(np.int16)
-# c=np.log(np.power(ab[:,:,0],2)+np.power(ab[:,:,1],2)+1e-8)
-# d=np.arctan2(ab[:,:,1],ab[:,:,0])
-# e=np.sqrt(np.exp(c))*np.exp(d)
-# sa=np.transpose(ab[:,:,0],(1,0))
-
 pl.subplot(4,1,1)
 aba=np.transpose(ab[1:,:,0],(1,0))
+aba[0,0]=10
 pl.imshow(aba,aspect="auto")
 pl.colorbar()
 pl.subplot(4,1,2)
-abn=np.transpose(ab[1:,:,0],(1,0))
+abn=np.transpose(ab[1:,:,1],(1,0))
+abn[0,0]=10
 pl.imshow(abn,aspect="auto")
 pl.colorbar()
 pl.subplot(4,1,3)
@@ -165,5 +171,3 @@ ww.setframerate(RATE)
 ww.writeframes(b.tobytes())
 ww.close()
 pl.show()
-a=np.asarray([88],dtype=np.float32)
-print(np.exp(a))
