46e9593efb61c3586f17e0d3fc539f801a7a54bd
toda <tadoproject@gmail.com>
2018-08-10 18:00:23 +0900

    [3.9.928] パラメータアップデート
    [fix] パラメータの調整

--- VRC/train/Model/model_proto.py
+++ VRC/train/Model/model_proto.py
@@ -622,7 +622,7 @@ def discriminator(inp,reuse,depth,chs,train=True):
     current=inp
     for i in range(depth):
         stddevs=math.sqrt(2.0/(16*chs[i]))
-        ten = tf.layers.conv2d(current, chs[i], kernel_size=[2,7], strides=[1,4], padding="VALID",kernel_initializer=tf.truncated_normal_initializer(stddev=stddevs), data_format="channels_last",name="disc_"+str(i),reuse=reuse)
+        ten = tf.layers.conv2d(current, chs[i], kernel_size=[2,5], strides=[1,2], padding="VALID",kernel_initializer=tf.truncated_normal_initializer(stddev=stddevs), data_format="channels_last",name="disc_"+str(i),reuse=reuse)
         if i%2==0:
             ten = tf.layers.batch_normalization(ten, axis=3, trainable=True, training=train, reuse=reuse,
                                                 name="bn_disc" + str(i))
