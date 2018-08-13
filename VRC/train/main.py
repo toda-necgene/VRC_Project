2bbc65e866b39e8980e7296e1b4b362b23a79e5c
toda <tadoproject@gmail.com>
2018-08-13 14:51:07 +0900

    [4.0.5] パラメータの調整
    [add] パラメータ

--- VRC/train/main.py
+++ VRC/train/main.py
@@ -2,7 +2,7 @@ import sys
 debug=False
 if __name__ == '__main__':
     pass
-from model.model_proto import Model  as model
+from model.model_train import Model  as model
 path="./setting.json"
 if len(sys.argv)!=0 and sys.argv.__contains__("--debug"):
     debug=True
