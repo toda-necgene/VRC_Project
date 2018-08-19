d7c3b830e0a8be09ac9164830fa361ec18b8aa4d
toda <tadoproject@gmail.com>
2018-08-19 10:20:15 +0900

    [4.1.70]　ハイパーパラメータを修正
    [fix] Gのアーキテクチャを変更

--- VRC/model/model.py
+++ VRC/model/model.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 def discriminator(inp,reuse,depth,chs,train=True):
     current=inp
     for i in range(depth):
-        ten = tf.layers.conv2d(current, chs[i], kernel_size=[2,5], strides=[1,2], padding="VALID",kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),use_bias=False, data_format="channels_last",name="disc_"+str(i),reuse=reuse)
+        ten = tf.layers.conv2d(current, chs[i], kernel_size=[2,7], strides=[1,2], padding="VALID",kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),use_bias=False, data_format="channels_last",name="disc_"+str(i),reuse=reuse)
         # ten = tf.layers.batch_normalization(ten, axis=3, training=train, trainable=True, reuse=reuse,
         #                                     name="bnD" + str(i))
-        ten=tf.layers.dropout(ten,0.4)
+        ten=tf.layers.dropout(ten,0.25)
         current = tf.nn.leaky_relu(ten)
-    # print(" [*] bottom shape:"+str(current.shape))
+    print(" [*] bottom shape:"+str(current.shape))
     h4=tf.reshape(current, [-1,current.shape[1]*current.shape[2]*current.shape[3]])
     ten=tf.layers.dense(h4,1,name="dence",reuse=reuse)
     return ten
@@ -78,7 +78,7 @@ def block_res(current,chs,rep_pos,depth,reuses,d,train=True):
     tenM=list()
     tms=len(d)
     for i in range(times):
-        tenA = tf.layers.conv2d(ten, chs[i + tms], kernel_size=[1, 2**(i+1)], strides=[1, 2**(i+1)], padding="VALID",
+        tenA = tf.layers.conv2d(ten, chs[i + tms], kernel_size=[1, 4], strides=[1, 4], padding="VALID",
                                 kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),use_bias=False,
                                 data_format="channels_last", reuse=reuses, name="convSmaller"+str(i) + str(rep_pos),
                                 dilation_rate=(1, 1))
@@ -116,7 +116,7 @@ def block_res(current,chs,rep_pos,depth,reuses,d,train=True):
     tms+=res
     for i in range(times):
         ten += tenM[times-i-1][:, -8:, :, :int(ten.shape[3])]
-        ten = deconve_with_ps(ten, [1, 2**(times-i)], chs[tms+i], rep_pos, reuses=reuses, name="00"+str(i))
+        ten = deconve_with_ps(ten, [1, 4], chs[tms+i], rep_pos, reuses=reuses, name="00"+str(i))
         ten = tf.layers.batch_normalization(ten, axis=3, training=train, trainable=True, reuse=reuses,
                                              name="bn"+str(times+res+i) + str(rep_pos))
         ten = tf.nn.relu(ten)
