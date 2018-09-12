d025019efdb8cb9576293efac747a5d1c1af48e0
toda <tadoproject@gmail.com>
2018-09-12 10:27:05 +0900

    [5.6.006] レイヤー数の変更
    [fix] 16レイヤーに変更

--- VRC/model/model.py
+++ VRC/model/model.py
@@ -43,16 +43,17 @@ def block_res(current,chs,rep_pos,depth,reuses,d,train=True):
     for i in range(res):
 
         tenA=ten
-        ten = tf.layers.conv2d(tenA, chs[tms + i]//2, [3, 5], [1, 2], padding="SAME",
+        ten = tf.layers.batch_normalization(ten, axis=3, training=train, trainable=True, reuse=reuses,
+                                            name="bnA1" + str(tms + i) + str(rep_pos))
+
+        ten = tf.layers.conv2d(ten, chs[tms + i]//2, [3, 5], [1, 2], padding="SAME",
                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.02), use_bias=True,
                                data_format="channels_last", reuse=reuses, name="res_conv1" + str(i) + str(rep_pos))
 
-        ten = tf.layers.batch_normalization(ten, axis=3, training=train, trainable=True, reuse=reuses,
-                                             name="bnA1"+str(tms+i) + str(rep_pos))
         ten = tf.nn.leaky_relu(ten)
         ten=tf.transpose(ten,[0,1,3,2])
         r=ten.get_shape()[-1]
-        ten=tf.layers.dense(ten, r,  reuse=reuses, name="res_dense" + str(i) + str(rep_pos))
+        ten=tf.layers.dense(ten, r,kernel_initializer=tf.random_normal_initializer(stddev=0.02),  reuse=reuses, name="res_dense" + str(i) + str(rep_pos))
         ten = tf.transpose(ten, [0, 1, 3, 2])
         ten = tf.layers.batch_normalization(ten, axis=3, training=train, trainable=True, reuse=reuses,
                                             name="bnA2" + str(tms + i) + str(rep_pos))
@@ -60,8 +61,6 @@ def block_res(current,chs,rep_pos,depth,reuses,d,train=True):
         ten = tf.layers.conv2d_transpose(ten, chs[tms + i], [3, 5], [1, 2], padding="SAME",
                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.02), use_bias=True,
                                data_format="channels_last", reuse=reuses, name="res_conv3" + str(i) + str(rep_pos))
-        ten = tf.layers.batch_normalization(ten, axis=3, training=train, trainable=True, reuse=reuses,
-                                             name="bnA3"+str(tms+i) + str(rep_pos))
         prop=(1-i/(res*2))
         ten=ShakeShake(ten,prop,train)
         # ten=tf.layers.dropout(ten,0.2,training=train)
