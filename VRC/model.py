a42ff7cb6540c3c9193976b86fc1ea3c2b054f4a
toda <tadoproject@gmail.com>
2019-01-16 08:35:39 +0900

    [19.1.16.3] GLU排除ResNet

--- VRC/model.py
+++ VRC/model.py
@@ -10,16 +10,19 @@ def discriminator(inp,reuse,train=True):
     """
     ten = tf.transpose(inp,[0,1,3,2])
 
-    w=tf.get_variable("kernel_e",[3,1,513,3],initializer=tf.initializers.random_normal(stddev=0.002))
+    w=tf.get_variable("kernel_e",[7,1,513,3],initializer=tf.initializers.random_normal(stddev=0.002))
     b=tf.get_variable("bias_e",[3],initializer=tf.zeros_initializer())
-    alpha=tf.get_variable("alpha_e",[1,1,1,3],initializer=tf.initializers.random_normal(stddev=0.002))
+    alpha=tf.get_variable("alpha_e",[3],initializer=tf.initializers.random_normal(stddev=0.002))
     w=tf.tanh(w)
     tenE=tf.nn.conv2d(ten,w,[1,1,1,1],"SAME")
-    tenE=tf.reshape(tf.exp(tf.nn.bias_add(tenE,b))*alpha,[ten.shape[0],ten.shape[1],3])
-
-    ten = tf.layers.conv2d(ten,256,[4,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=True,name="disc_0",reuse=reuse)
+    tenE=tf.reshape(tf.exp(tf.nn.bias_add(tenE,b))*alpha,[tenE.shape[0],tenE.shape[1],3])
+    ten = tf.layers.conv2d(ten,512,[4,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=False,name="disc_1",reuse=reuse)
+    ten = tf.contrib.layers.instance_norm(ten, reuse=reuse, scope="bn_1")
     ten = tf.nn.leaky_relu(ten)
-    ten = tf.layers.conv2d(ten,3,[4,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=True,name="disc_1",reuse=reuse)
+    ten = tf.layers.conv2d(ten,256,[3,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=False,name="disc_2",reuse=reuse)
+    ten = tf.contrib.layers.instance_norm(ten, reuse=reuse, scope="bn_2") 
+    ten = tf.nn.leaky_relu(ten)
+    ten = tf.layers.conv2d(ten,3,[1,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=True,name="disc_3",reuse=reuse)
 
     ten=tf.reshape(ten,[ten.shape[0],ten.shape[1],3])
     ten+=tenE
@@ -33,16 +36,17 @@ def generator(ten,reuse):
     """
 
     ten=tf.transpose(ten,[0,1,3,2])
-    ten = tf.layers.conv2d_transpose(ten, 64, [1, 1],kernel_initializer=tf.initializers.random_normal(stddev=0.002), use_bias=False,
+    ten = tf.layers.conv2d_transpose(ten, 64, [1, 8],kernel_initializer=tf.initializers.random_normal(stddev=0.002), use_bias=False,
                             reuse=reuse,name="encode_conv_")
     ten = tf.contrib.layers.instance_norm(ten, reuse=reuse, scope="bn_00")
     ten = tf.nn.leaky_relu(ten)
-    for i in range(2):
-        tenA = tf.layers.conv2d(ten,64,[4,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=False,reuse=reuse, name="mid_GATE_" + str(i))
-        tenA = tf.contrib.layers.instance_norm(tenA, reuse=reuse, scope="bn0" + str(i))
-        ten = ten*tf.nn.sigmoid(tenA)
-        ten = tf.layers.conv2d(ten,64,[8,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=False,reuse=reuse, name="mid_CONV_" + str(i))
-        ten = tf.contrib.layers.instance_norm(ten, reuse=reuse, scope="bn1" + str(i))
-    ten = tf.layers.conv2d(ten,513,[1,1],kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=True,reuse=reuse,name="decode_conv")
+    for i in range(6):
+        tenA = tf.layers.conv2d(ten,16,[4,3],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=False,reuse=reuse, name="mid_CONV_" + str(i))
+        tenA = tf.contrib.layers.instance_norm(tenA, reuse=reuse, scope="bn0_" + str(i))
+        tenA = tf.nn.leaky_relu(tenA)
+        tenA = tf.layers.conv2d(tenA,64,[4,1],[1,1],"SAME",kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=False,reuse=reuse, name="mid_CONV2_" + str(i))
+        tenA = tf.contrib.layers.instance_norm(tenA, reuse=reuse, scope="bn1_" + str(i))
+        ten=ten+tenA
+    ten = tf.layers.conv2d(ten,513,[1,8],kernel_initializer=tf.initializers.random_normal(stddev=0.002),use_bias=True,reuse=reuse,name="decode_conv")
     ten = tf.transpose(ten, [0, 1, 3, 2])
     return tf.tanh(ten)
